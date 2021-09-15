6692b7582a83771ceeb452ae5b7eb235529faf56
gunner10z-school <24jzweiback@athenian.org>
2021-09-15 14:25:17 -0700

    first

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,7 +5,12 @@ public class MyMain {
     // it is returned unchanged. 
     public static char toLower(char ch) {
         // REPLACE WITH YOUR CODE
-        return '!';
+        if (ch>='A'&&ch<='Z'){
+            return (char)(ch+=32);
+        }
+        else{
+            return ch;
+        }
     }
 
     // Changes a lowercase letter to an uppercase letter
@@ -13,19 +18,35 @@ public class MyMain {
     // it is returned unchanged.
     public static char toUpper(char ch) {
         // REPLACE WITH YOUR CODE HERE
-        return '!';
+
+        if (ch>='a'&&ch<='z'){
+            return (char)(ch-=32);
+        }
+        else{
+            return ch;
+        }
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        String newStr="";
+        for(int i = 0; i<str.length(); i++) {
+
+            // access each character
+            char a = str.charAt(i);
+            if (a<'0'||a>'9') {
+                newStr += a;
+            }
+        }
+        return newStr;
     }
 
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+        System.out.println(toLower('A'));
+        System.out.println(toUpper('a'));
+        System.out.println(removeNumbers("asdf1234"));
     }
 }
